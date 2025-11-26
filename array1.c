39e75d3593049dd2e08e3da163b3a45b2f9cd6c1
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-11-26 15:55:32 +0530

    print all value withou loop

--- array1.c
+++ array1.c
@@ -4,7 +4,10 @@ int main()
 {
 	int arr[5] = {1,2,3,4,5};
 	
+	printf("Value at index 0 = %d\n",arr[0]);
+	printf("Value at index 1 = %d\n",arr[1]);
+	printf("Value at index 2 = %d\n",arr[2]);
 	printf("Value at index 3 = %d\n",arr[3]);
-	printf("Value at index 0 = %d\n", arr[0]);
+	printf("Value at index 4 = %d\n",arr[4]);
 	return 0;
 }
