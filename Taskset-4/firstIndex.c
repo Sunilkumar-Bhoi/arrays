9ce7f0550adb4a7d2f7eed74cd7b87320ec1be38
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-11-29 12:18:36 +0530

    firstIndex

---
+++ Taskset-4/firstIndex.c
@@ -0,0 +1,14 @@
+//program to find first index of an element in an array
+#include <stdio.h>
+
+int main(){
+    int arr[7] = {5, 9, 5, 2, 5, 7, 5};
+    int target=5;
+
+    for(int i=0; i<7; i++){
+        if(arr[i]==target){
+            printf("First index of %d is: %d\n", target, i);
+            break;
+        }
+    }
+}
